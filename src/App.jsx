b5487ef63e76efdbb4ea56fac98863206a37c7b2
elisha <elishamutang@outlook.com>
2024-11-18 23:35:50 +0800

    [update] Added fetch request to Giphy to display Friends GIFs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 import Header from "./components/Header";
@@ -6,11 +6,38 @@ import Footer from "./components/Footer";
 
 function App() {
   const [score, setScore] = useState(0);
+  const [images, setImages] = useState([]);
+
+  const endPoint = new URL("https://api.giphy.com/v1/gifs/search");
+  endPoint.searchParams.append("api_key", "XKelAPHESxBXFhHXS8Hv8Ou4VzWbEbhy");
+  endPoint.searchParams.append("q", "friends");
+  endPoint.searchParams.append("limit", "12");
+
+  useEffect(() => {
+    fetch(endPoint)
+      .then((res) => res.json())
+      .then((dataObj) => {
+        setImages(() => {
+          const imagesCopy = [...dataObj.data];
+
+          return imagesCopy;
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(images);
 
   return (
     <>
       <Header score={score} />
-      <main></main>
+      <main>
+        {images.map((item) => {
+          return <img key={item.id} src={item.images.original.webp} className="image" />;
+        })}
+      </main>
       <Footer />
     </>
   );
