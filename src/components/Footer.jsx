55f8e35907db0b5ca713b3c729054c5e2e25a1dd
elisha <elishamutang@outlook.com>
2024-11-21 00:19:22 +0800

    [update] Added link to GH profile.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import "../styles/Footer.css";
 export default function Footer() {
   return (
     <footer>
-      <h2>Made by elishamutang</h2>
+      <h2>
+        Made by <a href="https://github.com/elishamutang/">elishamutang</a>
+      </h2>
     </footer>
   );
 }
