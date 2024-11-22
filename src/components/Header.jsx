b25a9b6fcbc327d4fce7be5b0f29b53c5fa8f5af
elisha <elishamutang@outlook.com>
2024-11-23 02:09:31 +0800

    [update] Passed topScore to Header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import "../styles/Header.css";
 
-export default function Header({ score }) {
+export default function Header({ score, bestScore }) {
   return (
     <header className="header">
       {/* Insert Friends logo instead of typed out version. */}
       <h1>Friends Memory Game</h1>
       <div className="scoreboard">
         <p>Score: {score}</p>
-        <p>Best Score: 0</p>
+        <p>Best Score: {bestScore}</p>
       </div>
     </header>
   );
